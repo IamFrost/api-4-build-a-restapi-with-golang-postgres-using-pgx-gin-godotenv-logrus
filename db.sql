fc560014f0b6dd49f9bc717d160e3c38e0ddfcbe
IamFrost <nothing@nobody.nowhere>
2020-07-29 11:55:40 +0600

    Not needed in code, only for db table structure

---
+++ db.sql
@@ -0,0 +1,12 @@
+create table usersec(
+	userid varchar(255),
+	mainmenu varchar(255),
+	menuname varchar(255)
+);
+
+insert into usersec values('rony','Accounting Head Entry','ACCOUNTS');
+insert into usersec values('suman','Create User','ADMIN');
+insert into usersec values('rony','Card Entry','INVENTORY');
+insert into usersec values('rony','Purchase Product Search Details','RECEIVED GOODS');
+insert into usersec values('rony','Ledger Book','ACCOUNTS');
+insert into usersec values('alex','Unit Entry','INVENTORY');
